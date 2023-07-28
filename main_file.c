55f8288007165d4f42e2ff03d0212b3bf06e7e75
A8DE <abdssamad.gourchi@gmail.com>
2023-07-28 03:04:11 -0700

    C - SIMPLE SHELL

--- main_file.c
+++ main_file.c
@@ -1,45 +1,44 @@
 #include "shell.h"
 
 /**
- * main - Function Entry Point
- * @c: Arg Count
- * @v: Arg Vector
- * Return: Success 0, Error 1
+ * main - entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 on success, 1 on error
  */
-
-int main(int c, char **v)
+int main(int ac, char **av)
 {
-	info_t inf[] = { INFO_INIT };
-	int f = 2;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
 	asm ("mov %1, %0\n\t"
-			"add $3, %0"
-			: "=r" (f)
-			: "r" (f));
+		"add $3, %0"
+		: "=r" (fd)
+		: "r" (fd));
 
-	if (c == 2)
+	if (ac == 2)
 	{
-		f = open(v[1], O_RDONLY);
-		if (f == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
 			if (errno == EACCES)
 				exit(126);
 			if (errno == ENOENT)
 			{
-				_eputs(v[0]);
-				_eputs(": 0:  Cant Open ");
-				_eputs(v[1]);
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
 				_eputchar('\n');
 				_eputchar(BUF_FLUSH);
 				exit(127);
 			}
 			return (EXIT_FAILURE);
 		}
-		inf->readfd = f;
+		info->readfd = fd;
 	}
-	populate_env_list(inf);
-	read_history(inf);
-	hsh(inf, v);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
 	return (EXIT_SUCCESS);
 }
-
